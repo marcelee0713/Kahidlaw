f11310a3545d9830958c1449e453ac8437ba6494
Marcel Magbual <magbualmarcel@gmail.com>
2023-08-31 23:42:14 +0800

    Added foot steps
    This will sound different on other grounds.

---
+++ Assets/Scripts/Environment/FootstepsManager.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepsManager : MonoBehaviour
+{
+    public AudioSource footStepsAudioSource;
+
+    public AudioClip woodFootSteps;
+    public AudioClip tilesFootSteps;
+
+    public Joystick joystick;
+    private string stepped = "";
+
+
+
+    void Update()
+    {
+        if (joystick.Direction.normalized != Vector2.zero)
+        {
+            IsMoving();
+        }
+        else
+        {
+            footStepsAudioSource.Stop();
+            footStepsAudioSource.enabled = false;
+
+        }
+    }
+
+    void IsMoving()
+    {
+        footStepsAudioSource.enabled = true;
+
+        if (stepped == "wood")
+        {
+            footStepsAudioSource.clip = woodFootSteps;
+        }
+        else if (stepped == "tiles")
+        {
+            footStepsAudioSource.clip = tilesFootSteps;
+        }
+
+        if (!footStepsAudioSource.isPlaying)
+        {
+            footStepsAudioSource.Play();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("TileWood"))
+        {
+            stepped = "wood";
+        }
+        else if (collision.gameObject.CompareTag("TileFloor"))
+        {
+            stepped = "tiles";
+        }
+
+    }
+}
