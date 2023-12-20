b710cf7aa2f572a3baf5765017eee1cb6d336ba0
Marcel Magbual <magbualmarcel@gmail.com>
2023-12-21 01:20:09 +0800

    Applied the new map
    This only include when the Grid or outdoor is applied.

--- Assets/Scripts/PlayerScripts/BulletScripts.cs
+++ Assets/Scripts/PlayerScripts/BulletScripts.cs
@@ -7,11 +7,13 @@ public class BulletScripts : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log("Collided by: " + collision.collider.tag);
         Destroy(gameObject);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log("Triggered by: " + collision.tag);
         Destroy(gameObject);
     }
 }
