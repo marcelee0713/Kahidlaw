ac0ad28f7134a99313442e5722c532e94d299f0c
Marcel Magbual <magbualmarcel@gmail.com>
2023-12-27 19:41:40 +0800

    Added an input for username on the start.

--- Assets/Scripts/PlayerReferences/PlayerRefs.cs
+++ Assets/Scripts/PlayerReferences/PlayerRefs.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerRefs : MonoBehaviour
 {
+    [Header("User name")]
+    public TMP_InputField inputField;
+    public TextMeshProUGUI errorInputHandler;
+    public GameObject nameInputPanel;
+
     [Header("Chapter 2 Consequences")]
     public GameObject FidelAssistance;
     public GameObject IsabelsAloneDialogue;
 
+    private void Awake()
+    {
+        inputField = GameObject.Find("NameField").GetComponent<TMP_InputField>();
+    }
+
     private void Start()
     {
-        if(FidelAssistance  != null && IsabelsAloneDialogue != null)
+        if (FidelAssistance  != null && IsabelsAloneDialogue != null)
         {
             Chapter2ButterflyEffect();
+        }
+    }
+
+    public void StoreName()
+    {
+        string userName = inputField.text;
+
+        if (userName == "")
+        {
+            errorInputHandler.text = "Enter your name.";
+        }
+        else if (userName.Length < 2 )
+        {
+            errorInputHandler.text = "Name is too short";
+        }
+        else if (userName.Length >= 15)
+        {
+            errorInputHandler.text = "Name is too long.";
+        } 
+        else
+        {
+            errorInputHandler.text = "";
+            nameInputPanel.SetActive(false);
+            PlayerPrefs.SetString("Username", userName);
         }
     }
 
