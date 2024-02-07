98effeb78059eea6f9e233b2b3c4c1230c575982
Marcel Magbual <magbualmarcel@gmail.com>
2024-02-07 23:46:55 +0800

    Added the prologue of the Pre Colonial.
    I only added the AI Generated Images tho.

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -234,9 +234,7 @@ public class MainMenu : MonoBehaviour
                 ResetEra("Japanese-Era", "JapaneseEraCurrentTime", "FinishedJapaneseEra", "UploadedJapaneseEra");
                 ResetEra("MartialLaw-Era", "MartialLawEraCurrentTime", "FinishedMartialLawEra", "UploadedMartialLawEra");
 
-
-                // Make a function for the start of PreColonial;
-                levelLoader.LoadGame("PreColonialFullMap"); // Remove soon because this is just a testing scene for checking.
+                levelLoader.LoadGame("PreColonial-Chapter1-Prologue"); 
                 FadeOutMusic();
                 break;
             case Eras.Spanish:
